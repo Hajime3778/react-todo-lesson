86752092e384d79ec68dc137483761d9ff45af2f
Hajime Takeuchi <shadowthe8156@gmail.com>
2021-02-11 14:56:35 +0900

    axiosでTodo一覧取得まで

--- no-cra/src/views/TodoList/TodoList.tsx
+++ no-cra/src/views/TodoList/TodoList.tsx
@@ -1,27 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import { Todo } from '../../model/Todo';
 import { TodoItem } from '../../components/TodoItem';
 import styles from './TodoList.css';
 
-const todos: Todo[] = [
-  {
-    id: 0,
-    title: 'Title1',
-    description: 'Description1',
-  },
-  {
-    id: 1,
-    title: 'Title2',
-    description: 'Description2',
-  },
-  {
-    id: 2,
-    title: 'Title3',
-    description: 'Description3',
-  },
-];
-
 export const TodoList: React.FC = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const getTodoList = async () => {
+      const response = await Axios.get<Todo[]>('todos');
+      setTodoList(response.data);
+    };
+    getTodoList();
+  }, [setTodoList]);
+
   return (
     <>
       <div>
@@ -31,7 +24,7 @@ export const TodoList: React.FC = () => {
       <div>
         <button className={styles.todoAddButton}>Click Me!!!</button>
       </div>
-      {todos.map((todo) => {
+      {todoList.map((todo) => {
         return <TodoItem key={todo.id} todo={todo} />;
       })}
     </>

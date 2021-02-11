58dfb1e5005df991da2f06365879875729b7da19
Hajime Takeuchi <shadowthe8156@gmail.com>
2021-02-11 17:31:37 +0900

    追加処理まで作成

--- no-cra/src/views/TodoList/TodoList.tsx
+++ no-cra/src/views/TodoList/TodoList.tsx
@@ -1,28 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
+import React, { useEffect, useState, useContext } from 'react';
+import Axios, { AxiosResponse } from 'axios';
+import { TodoListContext } from 'src/context/TodoListContext';
 import { Todo } from 'src/model/Todo';
 import { TodoItem } from 'src/components/TodoItem';
 import styles from './TodoList.css';
 
 export const TodoList: React.FC = () => {
-  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const { todoList, setTodoList } = useContext(TodoListContext);
+  const [todo, setTodo] = useState<Todo>({
+    title: '',
+    description: '',
+  });
 
   useEffect(() => {
-    const getTodoList = async () => {
+    (async () => {
       const response = await Axios.get<Todo[]>('todos');
       setTodoList(response.data);
-    };
-    getTodoList();
+    })();
   }, [setTodoList]);
+
+  const changedTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newTodo = Object.assign({}, todo);
+    newTodo.title = e.target.value;
+    setTodo(newTodo);
+  };
+
+  const changedDescription = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newTodo = Object.assign({}, todo);
+    newTodo.description = e.target.value;
+    setTodo(newTodo);
+  };
+
+  const addClick = async () => {
+    const response = await Axios.post<Todo, AxiosResponse<string>>(
+      'todos',
+      todo
+    );
+
+    const newTodoList = todoList.slice();
+    const newTodo = Object.assign({}, todo);
+
+    newTodo.id = parseInt(response.data);
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  };
 
   return (
     <>
       <div>
-        <input className={styles.todoTitleInput} />
-        <textarea className={styles.todoDescriptionInput} />
+        <input className={styles.todoTitleInput} onChange={changedTitle} />
+        <textarea
+          className={styles.todoDescriptionInput}
+          onChange={changedDescription}
+        />
       </div>
       <div>
-        <button className={styles.todoAddButton}>Click Me!!!</button>
+        <button className={styles.todoAddButton} onClick={addClick}>
+          Click Me!!!
+        </button>
       </div>
       {todoList.map((todo) => {
         return <TodoItem key={todo.id} todo={todo} />;

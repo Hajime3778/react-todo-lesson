6b7bbd9a7d953a3f4f907fba9ca7f4bcfd76d3ed
Hajime Takeuchi <shadowthe8156@gmail.com>
2021-02-13 12:52:01 +0900

    UnitTest追加

---
+++ no-cra/test/TodoItem.test.tsx
@@ -0,0 +1,39 @@
+import { TodoItem } from '../src/components/TodoItem';
+import { Todo } from '../src/model/Todo';
+import '@testing-library/jest-dom';
+import { screen, render, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+afterEach(() => {
+  return cleanup();
+});
+
+describe('TodoItem components', () => {
+  it('matches snapshot', () => {
+    const todo: Todo = { title: 'test title', description: 'test description' };
+    const { asFragment } = render(<TodoItem todo={todo} />, {});
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it('render todo', () => {
+    const todo: Todo = {
+      title: 'test title',
+      description: 'test description',
+    };
+    render(<TodoItem todo={todo} />);
+    expect(screen.getByText('test title')).toBeInTheDocument();
+    expect(screen.getByText('test description')).toBeInTheDocument();
+  });
+  it('click todo', () => {
+    const mockFunction = jest.fn();
+    const todo: Todo = {
+      title: 'test title',
+      description: 'test description',
+    };
+    render(<TodoItem todo={todo} onClick={mockFunction} />);
+    const todoItem = screen.getByText('test title')
+      .parentElement as HTMLElement;
+    userEvent.click(todoItem);
+    expect(mockFunction).toHaveBeenCalledTimes(1);
+  });
+});
